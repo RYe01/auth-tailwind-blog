7d402ddea9d15af38a15fc13e0c664b11cc405bf
RYe01 <rozs0001@hz.nl>
2021-06-17 14:15:38 +0200

    chaning config.php to https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tailwind-auth-blog.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
